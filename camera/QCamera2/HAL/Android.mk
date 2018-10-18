cae5441a0a8a89073fb5e827e77b316b95e3e97b
sanchitrohilla <sanchitrohilla64@gmail.com>
2018-10-18 19:09:34 +0000

    Camera: Restore pathmaps

--- camera/QCamera2/HAL/Android.mk
+++ camera/QCamera2/HAL/Android.mk
@@ -55,11 +55,10 @@ LOCAL_C_INCLUDES := \
         ../../../nativebase/include/ \
         frameworks/native/libs/nativewindow/include/ \
         frameworks/native/include/media/openmax \
-        hardware/qcom/display/libgralloc \
-        hardware/qcom/display/libqdutils \
-        hardware/qcom/media/libstagefrighthw \
-        hardware/qcom/media-caf/msm8916/libstagefrighthw \
-        hardware/qcom/display-caf/msm8916/libqservice \
+        $(call project-path-for,qcom-display)/libgralloc \
+        $(call project-path-for,qcom-display)/libqdutils \
+        $(call project-path-for,qcom-display)/libqservice \
+        $(call project-path-for,qcom-media)/libstagefrighthw \
         system/media/camera/include \
         $(LOCAL_PATH)/../../mm-image-codec/qexif \
         $(LOCAL_PATH)/../../mm-image-codec/qomx_core \
@@ -75,7 +74,7 @@ endif
 LOCAL_C_INCLUDES += \
         $(TARGET_OUT_HEADERS)/qcom/display
 LOCAL_C_INCLUDES += \
-        hardware/qcom/display/libqservice
+        $(call project-path-for,qcom-display)/libqservice
 
 #ifeq ($(TARGET_USE_VENDOR_CAMERA_EXT),true)
 #LOCAL_C_INCLUDES += $(call project-path-for,qcom-display)/libgralloc
